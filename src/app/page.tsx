96c74920aca52ef658368880e9c32503edd89b0b
Harsh-svg988 <harshprabhakar305@gmail.com>
2025-01-28 02:04:41 +0530

    Integrate clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <div>Home</div>
-      <Button>Sign in</Button>
-    </>
+          <div className="m-10">
+          <SignedOut>
+           <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          </div>
+     
   );
 }
