7d2b6cc34812d2b02322c77100b32363bf9aa8c8
Harsh-svg988 <harshprabhakar305@gmail.com>
2025-02-05 19:06:31 +0530

    camera fix & setting UI

--- src/components/MeetingSetup.tsx
+++ src/components/MeetingSetup.tsx
@@ -15,12 +15,16 @@ function MeetingSetup({onSetupComplete}:{onSetupComplete:()=>void}) {
     if(!call) return null
 
 
-    useEffect(()=>{
-        if(isCameraDisabled) call.camera.disable();
+      useEffect(() => {
+        if (isCameraDisabled) call.camera.disable();
+        else call.camera.enable();
+      }, [isCameraDisabled, call.camera]);
+    
+      useEffect(() => {
+        if (isMicDisabled) call.microphone.disable();
         else call.microphone.enable();
-
-    },[isMicDisabled,call.microphone])
-
+      }, [isMicDisabled, call.microphone]);
+    
     const handleJoin = async()=>{
         await call.join()
         onSetupComplete()
