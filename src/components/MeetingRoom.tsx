cfafa437171a8e64f18cf49f4875e778e89aff49
Harsh-svg988 <harshprabhakar305@gmail.com>
2025-02-06 14:18:54 +0530

    Basic resizable component

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import { CallingState, useCallStateHooks } from '@stream-io/video-react-sdk';
+import { LoaderIcon } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from './ui/resizable';
 
 
 function MeetingRoom() {
+    const router = useRouter(); 
+    const [layout,setLayout] = useState<"grid"|"speaker">('speaker')
+    const [showParticipants,setShowPaticipants] = useState(false);
+
+    const { useCallCallingState } = useCallStateHooks();
+
+    const callingState = useCallCallingState();
+
+    if(callingState !== CallingState.JOINED){
+        return(
+            <div className="h-96 flex items-center justify-center">
+                <LoaderIcon className="size-6 animate-spin" />
+            </div>
+        )
+    }
+
+
 
     return (
-        <div>Meeting Room</div>
+        <>
+        <ResizablePanelGroup direction='horizontal'>
+            <ResizablePanel defaultSize={35} minSize={25} maxSize={100} className='relative'>
+                <h1>Video</h1>
+            </ResizablePanel>
+
+            <ResizableHandle withHandle/>
+            <ResizablePanel defaultSize={65} minSize={25}>
+                <h1>Code editor</h1>
+            </ResizablePanel>
+
+
+        </ResizablePanelGroup>
+        </>
     )
 }
 
