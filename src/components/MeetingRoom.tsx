765ba8e1f097a5fc74442cd5f658f4fefde7f839
Harsh-svg988 <harshprabhakar305@gmail.com>
2025-02-06 14:50:48 +0530

    done meeting room

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,14 +1,21 @@
-import { CallingState, useCallStateHooks } from '@stream-io/video-react-sdk';
-import { LoaderIcon } from 'lucide-react';
+import { CallControls, CallingState, CallParticipantsList, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from '@stream-io/video-react-sdk';
+import { LayoutListIcon, LoaderIcon, UsersIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from './ui/resizable';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+  } from "./ui/dropdown-menu";
+  import { Button } from './ui/button';
 
 
 function MeetingRoom() {
     const router = useRouter(); 
     const [layout,setLayout] = useState<"grid"|"speaker">('speaker')
-    const [showParticipants,setShowPaticipants] = useState(false);
+    const [showParticipants,setShowParticipants] = useState(false);
 
     const { useCallCallingState } = useCallStateHooks();
 
@@ -25,10 +32,58 @@ function MeetingRoom() {
 
 
     return (
-        <>
+        <div className="h-[calc(100vh-4rem-1px)]">
         <ResizablePanelGroup direction='horizontal'>
             <ResizablePanel defaultSize={35} minSize={25} maxSize={100} className='relative'>
-                <h1>Video</h1>
+                {/* VIDEO LAYOUT */}
+                <div>
+                {layout === "grid" ? <PaginatedGridLayout/> :<SpeakerLayout/>}
+                    {/* PARTICIPANTS LIST OVERLAY */}
+                    {showParticipants && (
+                    <div className="absolute right-0 top-0 h-full w-[300px] bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+                        <CallParticipantsList onClose={() => setShowParticipants(false)} />
+                    </div>
+                    )}
+                </div>   
+                 
+                {/* VIDEO CONTROLS */}
+
+                <div className="absolute bottom-4 left-0 right-0">
+                    <div className="flex flex-col items-center gap-4">
+                    <div className="flex items-center gap-2 flex-wrap justify-center px-4">
+                        <CallControls onLeave={() => router.push("/")} />
+
+                        <div className="flex items-center gap-2">
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                            <Button variant="outline" size="icon" className="size-10">
+                                <LayoutListIcon className="size-4" />
+                            </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                            <DropdownMenuItem onClick={() => setLayout("grid")}>
+                                Grid View
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => setLayout("speaker")}>
+                                Speaker View
+                            </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+
+                        <Button
+                            variant="outline"
+                            size="icon"
+                            className="size-10"
+                            onClick={() => setShowParticipants(!showParticipants)}
+                        >
+                            <UsersIcon className="size-4" />
+                        </Button>
+
+                        {/* <EndCallButton /> */}
+                        </div>
+                    </div>
+                    </div>
+                </div>
             </ResizablePanel>
 
             <ResizableHandle withHandle/>
@@ -38,7 +93,7 @@ function MeetingRoom() {
 
 
         </ResizablePanelGroup>
-        </>
+        </div>
     )
 }
 
