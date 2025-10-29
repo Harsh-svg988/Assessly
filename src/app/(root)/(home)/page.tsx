d26555827600ddb178c683f96aecf26a1773d81f
Harsh-svg988 <harshprabhakar305@gmail.com>
2025-10-30 00:29:01 +0530

    feat: Scheduling Interviews

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -11,6 +11,7 @@ import { api } from "../../../../convex/_generated/api";
 import { useRouter } from "next/navigation";
 import Meeting from "../meeting/[id]/page";
 import MeetingModal from "@/components/MeetingModal";
+import LoaderUI from "@/components/LoaderUI";
 
 export default function Home() {
   const { isInterviewer, isCandidate, isLoading } = useUserRole();
@@ -38,7 +39,7 @@ export default function Home() {
 
 
   }
-  if(isLoading) return <p>Loading...</p>
+  if(isLoading) return <LoaderUI/>
   
   return (
     <div className="container max-w-7xl mx-auto p-6">
